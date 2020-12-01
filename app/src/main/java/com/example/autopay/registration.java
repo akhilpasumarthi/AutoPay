e66dbd87adb623fbece5d793e4d59f120b770c95
prashanth8520 <66008694+prashanth8520@users.noreply.github.com>
2020-12-01 23:14:52 +0530

    done some changes on login,registration

--- app/src/main/java/com/example/autopay/registration.java
+++ app/src/main/java/com/example/autopay/registration.java
@@ -1,4 +1,5 @@
 package com.example.autopay;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -9,21 +10,21 @@ import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView register;
+public class registration extends AppCompatActivity {
+    TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        register=findViewById(R.id.register);
-        register.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_registration);
+        login=findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String mystring=new String("Register");
+                String mystring=new String("Login");
                 SpannableString content = new SpannableString(mystring);
                 content.setSpan(new UnderlineSpan(), 0, mystring.length(), 0);
-                register.setText(content);
-               startActivity(new Intent(MainActivity.this,registration.class));
+                login.setText(content);
+                startActivity(new Intent(registration.this,MainActivity.class));
             }
         });
     }
