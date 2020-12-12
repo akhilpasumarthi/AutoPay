1658d48f161c7788ba283f1b11a3da9aca3668e6
Prasanna <prasannabondalapati20@gmail.com>
2020-12-12 21:28:15 +0530

    Notifications with cloud messaging

---
+++ app/src/main/java/com/example/notification1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.notification1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+        }
+        
