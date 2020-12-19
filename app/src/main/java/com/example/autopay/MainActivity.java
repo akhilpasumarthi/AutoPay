a1ebd804f2d0e93d4b5dd2374a2d86abc53d66b1
prashanth8520 <66008694+prashanth8520@users.noreply.github.com>
2020-12-19 20:36:36 +0530

    changes in design

--- app/src/main/java/com/example/autopay/MainActivity.java
+++ app/src/main/java/com/example/autopay/MainActivity.java
@@ -1,84 +1,91 @@
 package com.example.autopay;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    TextView register,dummy;
-    EditText username,password;
+    private Spinner spinner;
+    private EditText editText;
+    TextView dummy;
+    EditText username;
     Button userlogin;
     Button payment;
-    FirebaseFirestore fstore;
+   // FirebaseFirestore fstore;
     String userid;
-    FirebaseAuth fauth;
+    //FirebaseAuth fauth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        register=findViewById(R.id.register);
-        username=findViewById(R.id.username);
-        password=findViewById(R.id.password);
-        userlogin=findViewById(R.id.userlogin);
-        fauth=FirebaseAuth.getInstance();
-        fstore=FirebaseFirestore.getInstance();
+        //number=findViewById(R.id.editTextPhone);
+        //userlogin=findViewById(R.id.buttonContinues);
+        //fauth=FirebaseAuth.getInstance();
+        //fstore=FirebaseFirestore.getInstance();
         dummy=findViewById(R.id.dummy);
         payment=findViewById(R.id.ether);
+
+        spinner = findViewById(R.id.spinnerCountries);
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,CountryData.countryNames));
+
+        editText = findViewById(R.id.editTextPhone);
+
+        findViewById(R.id.buttonContinues).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = CountryData.countryAreaCodes[spinner.getSelectedItemPosition()];
+
+                String number = editText.getText().toString().trim();
+
+                if (number.isEmpty() || number.length() < 10) {
+                    editText.setError("Valid number is required");
+                    editText.requestFocus();
+                    return;
+                }
+
+                String phonenumber = "+" + code + number;
+
+                Intent intent = new Intent(MainActivity.this, otpauthentication.class);
+                intent.putExtra("phonenumber", phonenumber);
+                startActivity(intent);
+
+            }
+        });
+
+
+
         dummy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,Dashboard.class));
             }
         });
-        userlogin.setOnClickListener(new View.OnClickListener() {
-            String user=username.getText().toString().trim();
-            String pass=password.getText().toString().trim();
-            @Override
-            public void onClick(View v) {
-               // userid=fauth.getCurrentUser().getUid();
-                DocumentReference documentReference=fstore.collection("userd").document("users");
-                Map<String,Object> user=new HashMap<>();
-                user.put("username","hii");
-                user.put("password","hello");
-                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String mystring=new String("Register");
-                SpannableString content = new SpannableString(mystring);
-                content.setSpan(new UnderlineSpan(), 0, mystring.length(), 0);
-                register.setText(content);
-                startActivity(new Intent(MainActivity.this,registration.class));
-            }
-        });
+
+
         payment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,ethereum.class));
             }
         });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/autopay/registration.java
+++ app/src/main/java/com/example/autopay/registration.java
@@ -15,60 +15,13 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class registration extends AppCompatActivity {
-    private Spinner spinner;
-    private EditText editText;
+
     TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        login=findViewById(R.id.login);
-        spinner = findViewById(R.id.spinnerCountries);
-        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,CountryData.countryNames));
 
-        editText = findViewById(R.id.editTextPhone);
-
-        findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String code = CountryData.countryAreaCodes[spinner.getSelectedItemPosition()];
-
-                String number = editText.getText().toString().trim();
-
-                if (number.isEmpty() || number.length() < 10) {
-                    editText.setError("Valid number is required");
-                    editText.requestFocus();
-                    return;
-                }
-
-                String phonenumber = "+" + code + number;
-
-                Intent intent = new Intent(registration.this, otpauthentication.class);
-                intent.putExtra("phonenumber", phonenumber);
-                startActivity(intent);
-
-            }
-        });
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String mystring=new String("Login");
-                SpannableString content = new SpannableString(mystring);
-                content.setSpan(new UnderlineSpan(), 0, mystring.length(), 0);
-                login.setText(content);
-                startActivity(new Intent(registration.this,MainActivity.class));
-            }
-        });
     }
-    @Override
-    protected void onStart() {
-        super.onStart();
 
-        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-            startActivity(intent);
-        }
-    }
 }
