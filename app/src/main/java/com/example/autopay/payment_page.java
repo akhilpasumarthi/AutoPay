4bedd91e546be781bddfe97c931853eab02b9ee3
vamsidhar1999 <51114673+vamsidhar1999@users.noreply.github.com>
2020-12-25 20:02:01 +0530

    Commit

--- app/src/main/java/com/example/autopay/payment_page.java
+++ app/src/main/java/com/example/autopay/payment_page.java
@@ -10,10 +10,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class payment_page extends AppCompatActivity {
     String toaddress;
     EditText payamount,smsg;
     Button send;
+    String to_address = "";
+    String to_user = "";
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +35,8 @@ public class payment_page extends AppCompatActivity {
         payamount=findViewById(R.id.sendamount);
         smsg=findViewById(R.id.smsg);
         send=findViewById(R.id.send);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
         Log.i("toaddress",toaddress);
         send.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,8 +47,30 @@ public class payment_page extends AppCompatActivity {
                     String net = e.connectToEthNetwork(v);
                     //Toast.makeText(getApplicationContext(), net, Toast.LENGTH_LONG).show();
                     String address = e.sendTransaction(v,amt, toaddress);
-                    Toast.makeText(getApplicationContext(),"Transaction Completed", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(payment_page.this,Dashboard.class));
+                    firebaseFirestore.collection("users")
+                            .document(firebaseAuth.getCurrentUser().getUid())
+                            .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            to_user = documentSnapshot.getString("name");
+                            to_address = documentSnapshot.getString("walletaddress");
+                        }
+                    });
+                    DocumentReference documentReference=firebaseFirestore.collection("users")
+                            .document(firebaseAuth.getCurrentUser().getUid())
+                            .collection("transactions").document();
+                    Map<String,Object> payments=new HashMap<>();
+                    payments.put("from_wallet", to_address);
+                    payments.put("name", to_user);
+                    payments.put("amount", amt);
+                    payments.put("transactionhash",address);
+                    documentReference.set(payments).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(getApplicationContext(),"Transaction Completed", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(payment_page.this,Dashboard.class));
+                        }
+                    });
                 }
                 catch (Exception e){
                     String e1=e.toString();
